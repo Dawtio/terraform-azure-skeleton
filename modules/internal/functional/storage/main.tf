ec098bd46f86cabfdb75fc72725567e6a246b41c
Maxime Brunet <mbrunet@dawtio.cloud>
2021-09-23 17:54:52 +0200

    feat: update storage account names

--- modules/internal/functional/storage/main.tf
+++ modules/internal/functional/storage/main.tf
@@ -9,7 +9,7 @@ module "storage_accounts" {
   for_each = local.storage_accounts
   source   = "../../base/azurerm-storage-account"
 
-  name                = each.key
+  name                = "${each.key}-${var.context.environment_code}"
   resource_group_name = var.context.resource_group.name
   location            = var.context.resource_group.location
 }
